4aa1cc64ce132cd26d73d5ccd3616fbefcb2036f
rajan.rwl <rajan.rwl@10233137-f34e-c8b6-510c-b2f21fc86211>
2011-12-27 07:39:48 +0000

    Ajax file upload, db to sql, mimecheck

--- mimecheck/index.php
+++ mimecheck/index.php
@@ -5,5 +5,22 @@
 		<input type="file" name="file" id="file" /><br />
 		<input type="submit" name="submit" value="Submit" />
 	</form>
+	<?php
+		function printCase($case='two'){
+			switch ($case){
+				case 'three':
+					echo "first Case is :";
+					break;
+				case 'two':
+					echo "Second Case is :";
+					break;
+				case 'one':
+					echo "Third Case is :";
+					break;
+			}	
+		}
+
+		printCase();
+	?>
 	</body>
-</html> 
+</html>
